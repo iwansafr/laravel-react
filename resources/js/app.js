991a089c55496d71ae37fd92f9e86e306c2b2c68
iwansafr <iwansafr@gmail.com>
2022-12-27 13:52:27 +0700

    preset react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+
+import App from './components/App';
+console.log('test')
+ReactDOM(document.getElementById('app')).render(
+    <App />
+)
